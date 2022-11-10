a1b6bebb9f0024d3bb0562daee740b0d0b14b3e3
DimasAuliaNaufal <101788695+DimasAuliaNaufal@users.noreply.github.com>
2022-11-10 22:32:09 +0700

    commit function

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,16 +1,3 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-// ignore_for_file: prefer_typing_uninitialized_variables
-
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_pertama/main.dart';
 
 void main() {
     // tipe data dan variabel
@@ -98,7 +85,50 @@ print(mahasiswaString);
   print("------------");
   nilai >= 80 ? print("A") : print("Tidak A");
 
+   //function
+  print("Function");
 
-
+  hitungNilai();
+  hitungNilai1(75, 90);
+  var p = hitungNilai1(2, 50);
+  print(p);
+  var n = hitungNilai2(mapel1: 50, mapel2: 2);
+  print(n);
+  var o = hitungNilai3(79, 100);
 
 }
+
+
+//function
+hitungNilai() {
+  print("Hitung Nilai");
+}
+
+//positional argumen
+hitungNilai1(mapel1, mapel2, [mapel3]) {
+  var nilaiAkhir;
+  if(mapel3 != null){
+    nilaiAkhir = mapel1 / mapel2 + mapel3;
+  }else{
+    nilaiAkhir = mapel1 / mapel2;
+  }
+  return nilaiAkhir;
+}
+
+//name argumen
+hitungNilai2({mapel1, mapel2}) {
+  var nilaiAkhir;
+  if(mapel2 != null){
+    nilaiAkhir = mapel1 / mapel2;
+  }else{
+    nilaiAkhir = mapel1;
+  }
+  return nilaiAkhir;
+}
+
+//void
+void hitungNilai3(mapel1, mapel2) {
+  var nilaiAkhir = mapel1 + mapel2;
+  print(nilaiAkhir);
+}
+
